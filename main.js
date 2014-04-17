458b984edd842b21548da0d24fe9d9b0769c0c1c
Kelsey Brisson <klbrisson@gmail.com>
2014-04-17 15:08:45 -0600

    Made markers scale with map as screen size changes. Fixed focus on the textarea for notes.

--- main.js
+++ main.js
@@ -2,74 +2,102 @@ $(document).on('ready', function() {
 	var topPosition;
 	var leftPosition;
 
-	//Create Marker Variable and Styling
+var inputBox = function (notecontainer) {
+	var input = $('<textarea class="input" placeholder="Add A Note!"></textarea>');
+ 	notecontainer.append(input);
+	// input.focus();
 
 
+	//input box position
+	input
+		.css('position','absolute');
+ 	
+
+ 	//Save note text and remove text area
+ 	input.on('blur', function() {
+	 	var noteText = $(this).val();
+	 	var saveNote = $('<div class="note">'+noteText+'</div>');
+
+	 	saveNote
+	 		.css('position','absolute');
+	 	notecontainer.append(saveNote);
+	 	$(this).remove();
+ 	});
+ };
 
 
-		var noteMarker = $("<div class='note-marker'></div>");
+var markerCreator = function () {
+	var marker = $("<div class='marker'></div>");	
+	var noteMarker = $("<div class='note-marker'></div>").append(marker);
+	noteMarker
+		.css('position','absolute')
+		.css('top',topPosition + '%')
+		.css('left',leftPosition + '%');
+	marker		
+		.width('10px')
+		.height('10px')
+		.css('background-color','#F00');
+		inputBox(noteMarker);
+ 	return noteMarker;
+};
+
+
 
 
  	$(document).on('click', '#map', function() {
- 		topPosition= event.pageY;
-	 	leftPosition= event.pageX;
+ 		// convert to percentages in order to scale
+ 		topPosition= (event.pageY/$('#map').height()) * 100;
+ 		console.log(event.pageY);
+ 		console.log(topPosition);
+	 	leftPosition= event.pageX/$('#map').width() * 100;
+	 	var newMarker = markerCreator();
+	 	$('.container').append(newMarker);
+	 	newMarker.find('textarea').focus();
+	});
 	
+		// // var noteMarker = $("<div class='note-marker'></div>");
 		// var noteMarker = $("<div class='note-marker'></div>");
 
- 		var marker = $("<div class='marker'></div>");	
-		marker		
-		.width('10px')
-		.height('10px')
-		.css('background-color','#F00')
-		.css('position','absolute')
-		.css('top',topPosition + 'px')
-		.css('left',leftPosition + 'px');
+ 	// 	var marker = $("<div class='marker'></div>");	
+		// marker		
+		// .width('10px')
+		// .height('10px')
+		// .css('background-color','#F00')
+		// .css('position','absolute')
+		// .css('top',topPosition + 'px')
+		// .css('left',leftPosition + 'px');
 	 		
-	 	$('.container').append(noteMarker).append(marker);
+	 // 	$('.container').append(noteMarker);
+	 // 	$('.note-marker').append(marker);
 
 	 	//Create text area  ;to do later change note to input
-	 	var note = $('<textarea class="note" placeholder="Add A Note!"></textarea>');
- 		$('.container').append(note);
-		$(".note").focus();
+	 // 	var input = $('<textarea class="input" placeholder="Add A Note!"></textarea>');
+ 	// 	$('.container').append(input);
+		// $(".input").focus();
 
-		//Note styling
-		note
-		.css('position','absolute')
-		.css('top',topPosition + 10 + 'px')
-		.css('left',leftPosition + 10 + 'px');
- 	});
+		// //Note styling
+		// input
+		// .css('position','absolute')
+		// .css('top',topPosition + 10 + 'px')
+		// .css('left',leftPosition + 10 + 'px');
+ 	// });
 
- 	//Save note text and remove text area
- 	$(document).on('blur','.note', function() {
- 		var noteText = $('.note').val();
- 		var saveNote = $('<div>'+noteText+'</div>');
+ 	// //Save note text and remove text area
+ 	// $(document).on('blur','.input', function() {
+ 	// 	var noteText = $('.input').val();
+ 	// 	var saveNote = $('<div>'+noteText+'</div>');
 
- 		saveNote
- 		.css('position','absolute')
-		.css('top',topPosition + 10 + 'px')
-		.css('left',leftPosition + 10 + 'px');
- 		$('.container').append(saveNote);
- 		$(this).remove();
- 		}
- 	);
+ 	// 	saveNote
+ 	// 	.css('position','absolute')
+		// .css('top',topPosition + 10 + 'px')
+		// .css('left',leftPosition + 10 + 'px');
+ 	// 	$('.container').append(saveNote);
+ 	// 	$(this).remove();
+ 	
 
  	
  	$(document).on('click','.marker', function () {
- 		$(this).remove();
- 	});
-
-
-
-
- 	$(document).on('click','.container', function() {
- 		$("#log").text("pageX: "+event.pageX + ", pageY: " + event.pageY);
- 	});
-
- 	$(document).click(function() {
- 		console.log("pageX: "+ event.pageX+"pageY: " +event.pageY);
- 		console.log(leftPosition +"  "+ topPosition );
- 		console.log(typeof leftPosition);
-
+ 		$(this).parent().remove();
  	});
 
 });
